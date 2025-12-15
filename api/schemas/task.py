fc41ce6c0973ea0ac41fe238f3a6156d90fc95d4
korezawa1005 <t.k.oshihiki@gmail.com>
2025-12-15 17:54:49 +0900

    fastapi0章

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -2,8 +2,24 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
+class TaskBase(BaseModel):
+    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
 
-class Task(BaseModel):
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
     id: int
-    title: Optional[bool] = Field(None, example="クリーニングを取りに行く")
+
+    class Config:
+        orm_mode = True
+
+
+class Task(TaskBase):
+    id: int
     done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+        orm_mode = True
